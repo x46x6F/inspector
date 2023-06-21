571c74c7ffee8a0cfa7f09f9f5f129ee1408bc61
Lisa <chalabi.lisa13@gmail.com>
2023-06-21 13:31:23 +0200

    Migrations pour les tables temporaires des csv

---
+++ inspector/database/migrations/2023_06_21_093820_create_csv_pieces_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('csv_pieces', function (Blueprint $table) {
+            $table->integer('constructor_id');
+            $table->integer('model_id');
+            $table->integer('type_id');
+            $table->string('model_name');
+            $table->integer('creation_year');
+            $table->boolean('has_electro');
+            $table->boolean('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('csv_pieces');
+    }
+};

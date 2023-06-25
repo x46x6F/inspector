a5ed64b42a0df989992158588e598649e6c945bb
manzoneflorianpro <manzoneflorianpro@gmail.com>
2023-06-25 17:03:22 +0200

    [UPDATE] Rename model Models en Model

--- inspector/database/seeders/DatabaseSeeder.php
+++ inspector/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Models;
+use App\Models\Model;
 use App\Models\Piece;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Models::factory(10)->create();
+        Model::factory(10)->create();
         Piece::factory(50)->create();
         User::factory(10)->create();
     }

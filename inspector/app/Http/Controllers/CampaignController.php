a6ba57289f1e93076f3a738f6442c9b94e234935
Lisa <chalabi.lisa13@gmail.com>
2023-06-26 15:15:21 +0200

    Controller Campagne

--- inspector/app/Http/Controllers/CampaignController.php
+++ inspector/app/Http/Controllers/CampaignController.php
@@ -12,57 +12,89 @@ class CampaignController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index() : \Inertia\Response
+    public function index(): \Inertia\Response
     {
-        $campaigns = Campaign::with(['user', 'sites', 'pieces'])->get();
-        return Inertia::render('Campaigns', ['campaigns' => $campaigns]);
+        $campaign = Campaign::with(['user', 'sites', 'pieces', 'materials', 'audits'])->get();
+        return Inertia::render('Campaign', ['campaign' => $campaign]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): \Inertia\Response
     {
-        //
+        return Inertia::render('campaign/Create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): \Illuminate\Http\RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required | max:255',
+            'status'=> 'required | max:255',
+            'start_date' => 'required | date',
+            'end_date' => 'required | date',
+        ]);
+
+        $campaign = new Campaign();
+        $campaign->name = $validated['name'];
+        $campaign->status = $validated['status'];
+        $campaign->start_date = $validated['start_date'];
+        $campaign->end_date = $validated['end_date'];
+        
+        $campaign->save();
+
+        return to_route('campaign.index');
+
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Campaign $campaigns) : \Inertia\Response
+    public function show(Campaign $campaign): \Inertia\Response
     {
-        return Inertia::render('Campaign/Show', ['campaigns' => $campaigns]);
+        return Inertia::render('Campaign/Show', ['campaign' => $campaign]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Campaign $campaign): \Inertia\Response
     {
-        //
+        return Inertia::render('Campaign/Edit', ['campaign' => $campaign]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Campaign $campaign): \Illuminate\Http\RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required | max:255',
+            'status'=> 'required | max:255',
+            'start_date' => 'required | date',
+            'end_date' => 'required | date',
+        ]);
+
+        $campaign->name = $validated['name'];
+        $campaign->status = $validated['status'];
+        $campaign->start_date = $validated['start_date'];
+        $campaign->end_date = $validated['end_date'];
+        
+        $campaign->save();
+
+        return to_route('campaign.show');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Campaign $campaign)
     {
-        //
+        $campaign->delete();
+        return $this->index();
     }
 }

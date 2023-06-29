0ffaa322614b8a0d7d3ae122691227bc275c06ec
manzoneflorianpro <manzoneflorianpro@gmail.com>
2023-06-29 12:05:36 +0200

    Merge flo into FlorianM

--- inspector/resources/js/Pages/Model/Material/Index.vue
+++ inspector/resources/js/Pages/Model/Material/Index.vue
@@ -1,37 +1,66 @@
 <script setup lang="ts">
 import Layout from '@/Layouts/Layout.vue';
 import DynamicTable from '@/Components/DynamicTable.vue';
+import OptionBar from '@/Components/OptionBar.vue';
+import SearchBar from '@/Components/SearchBar.vue';
+import UploadButton from '@/Components/UploadButton.vue';
 import TitlePage from '@/Components/TitlePage.vue';
-import { Head, usePage } from '@inertiajs/vue3';
+import { Head, router, usePage, useForm } from '@inertiajs/vue3';
 
 const page = usePage()
 
 const props = defineProps<{
-  csv_materials: Array<any>,
+  materials: Array<any>,
+  existingMaterials?: number
 }>()
 
 const head = {
+  id: 'ID',
+  name: 'Modèle',
+  status: 'Status',
   constructor_id: 'Constructeur',
-  model_id: 'Modèle',
   type_id: 'Type',
-  site_id: 'Site',
   creation_year: 'Année',
   has_electro: 'Electro',
-  status: 'Status',
+}
+
+const watcher = (search) => {
+  router.get(
+    "/models/materials",
+    { search: search },
+    { preserveState: true, replace: true }
+  )
+}
+
+const form = useForm({
+  file: ''
+})
+
+const sendFile = e => {
+  form.file = e.target.files[0]
+  form.post(route('models.materials.store'))
 }
 
 </script>
 
 <template>
   <Head title="Matériel" />
-    <Layout>
-      <TitlePage>
-        Référentiel Matériel
-      </TitlePage>
-      <DynamicTable :headers="head" />
-    </Layout>
+  <Layout>
+    <TitlePage>
+      Référentiel Matériel
+    </TitlePage>
+
+    <OptionBar>
+      <UploadButton v-if="page.props.auth?.user.canImportData" @change="sendFile" />
+      <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+        role="alert" v-if="$page.props.flash.success">
+        <p>{{ $page.props.flash.success }}</p>
+      </div>
+      <SearchBar @write="watcher" />
+    </OptionBar>
+
+    <DynamicTable :headers="head" :data="materials" />
+  </Layout>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
